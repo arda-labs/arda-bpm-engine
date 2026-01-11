d4f7dede8c7a5bc8a1ec535b3bb0455ecd123e4f
HoanIT <lehoan.dev@gmail.com>
2026-01-11 20:57:39 +0700

    Initial commit or update

---
+++ src/test/java/vn/io/arda/bpm/ArdaBpmEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.io.arda.bpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdaBpmEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
